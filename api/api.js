62e8081dc324ebdcb0eec1e0c65070412600c139
59366732 <ings59@email.nu.ac.th>
2022-07-30 22:56:42 +0700

    log response

--- api/api.js
+++ api/api.js
@@ -1,14 +1,19 @@
 import express from 'express';
 import chalk from 'chalk';
+import cookieParser from 'cookie-parser';
 const router = express.Router();
 
+router.use(cookieParser());
 router.get('/', async (req, res) => {
     try {
-        let responseData = "Get data has successfully";
-        res.json({status: 200, message: chalk.green(responseData)}); // for manage incomming request and then what server send to client
+        let text = "Get data has successfully";
+        res.json({status: 200, message: text}); // for manage incomming request and then what server send to client
+        console.log("Result : " + chalk.green(text.toString()));
+        console.log('Cookies: ', req.cookies);
+        console.log('Signed Cookies: ', req.signedCookies);
     } catch (error) {
-        console.log(error);
-        return res.status(500).send("Server error" + chalk.red(error));
+        console.log(chalk.green(error));
+        return res.status(500).send("Result : " + chalk.green(error));
     }
 });
-module.exports = router;
+export default router;
--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import express from 'express';// import express
 import chalk from 'chalk';
 import debug from 'debug';
-import api from './api/api';
-import morgan from 'morgan';
+import api from './api/api.js';
 const app = express(); // call express
 const PORT= process.env.PORT || 3000;
 
-app.use(morgan('combined')); //client machine  info
 app.use("/api/api", api);
 app.listen(PORT, () => {
     debug("Listening on port : " + chalk.green(PORT));
+    console.log(`Server is running on port ${chalk.green(PORT)}`);
 }) //tell server listen on port
