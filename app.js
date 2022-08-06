aeb235597137e56d978ef0d872922c52be8c8694
59366732 <ings59@email.nu.ac.th>
2022-08-06 21:47:51 +0700

    add ejs

--- app.js
+++ app.js
@@ -11,19 +11,19 @@ const PORT = process.env.PORT || 3000;
 import path from 'path';
 import {fileURLToPath} from 'url';
 const web_type = "dynamics";
-if (web_type === "statics") {
-    const __filename = fileURLToPath(import.meta.url);
-    const __dirname = path.dirname(__filename);
-    app.use(express.static(path.join(__dirname, "/public/")));
-} else if (web_type === "dynamics") {
-    const test = false;
-    if (test === true) {
-        app.use("/api/getjson", getjson);
-        app.use("/api/getejs", getejs);
-    }
-    app.use("/", indexjs);
-}
-
+// if (web_type === "statics") {
+//     const __filename = fileURLToPath(import.meta.url);
+//     const __dirname = path.dirname(__filename);
+//     app.use(express.static(path.join(__dirname, "/public/")));
+// } else if (web_type === "dynamics") {
+//     const test = false;
+//     if (test === true) {
+//         app.use("/api/getjson", getjson);
+//         app.use("/api/getejs", getejs);
+//     }
+//     app.use("/", indexjs);
+// }
+app.use("/", indexjs);
 app.listen(PORT, () => {
     debug("listening on port : " + chalk.green(PORT));
     console.log(`Server is running on port ${
