a80b3f125e207d8c7e385514c1b439870d4997d4
59366732 <ings59@email.nu.ac.th>
2022-08-07 22:12:37 +0700

    test

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 import express from 'express';
 const indexjs = express();
+const productRouter = express.Router();
 
 indexjs.set("views", "./src/views");
 indexjs.set("view engine", "ejs");
+
+
+productRouter.route("/").get((req, res) => {
+    res.render("products", {
+        products: [
+         {title: "A", description: "aaa"}, 
+         {title: "B", description: "bbb"},   
+         {title: "C", description: "ccc"},   
+         {title: "D", description: "ddd"},   
+        ],
+    });
+});
+indexjs.use("/products", productRouter);
+
+
 indexjs.get("/", (req, res) => {
     try {
         res.render('index', {username: "Ing Saeyang", customer: ['a', 'b', 'c']});
